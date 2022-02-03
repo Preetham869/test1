199291eab20f8af9091939690ab748fa7b75715a
Preetham869 <70512182+Preetham869@users.noreply.github.com>
2022-02-03 13:49:52 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
-export default class App extends React.Component() {
+export default class App extends React.Component {
   render() {
     return (
       <Login/>
