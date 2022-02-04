474919fa11f155a438fbf8a459856f3eb67e3724
Preetham869 <70512182+Preetham869@users.noreply.github.com>
2022-02-04 21:31:17 +0530

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,90 +1,202 @@
-import React from "react";
+import React from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    ImageBackground,
-    ScrollView,
-    TextInput,
-    TouchableOpacity
-} from "react-native";
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import { Feather } from '@expo/vector-icons';
-import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
 
 let customFonts = {
-    OldEnglish: require('../assets/OldeEnglish.ttf'),
-  };
-  
+  OldEnglish: require('../assets/OldeEnglish.ttf'),
+};
+
 export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          fontsLoaded: false,
-        };
-      }
-    
-      async _loadFontsAsync() {
-        await Font.loadAsync(customFonts);
-        this.setState({ fontsLoaded: true });
-      }
-    
-      componentDidMount() {
-        this._loadFontsAsync();
-      }
-    
-      render() {
-        if (!this.state.fontsLoaded) {
-          return <AppLoading />;
-        } else {
-        return (
-            <View style={{ flex: 1 }}>
-                <ScrollView>
-                    <Image
-                        source={require("../assets/Logo.png")}
-                        style={{ width: "80%", height: 250, marginTop: "20%", alignSelf: 'center', resizeMode: 'contain' }}
-                    />
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+    };
+  }
 
-                    {/* <ImageBackground
-                        source={require("../assets/musicBackground.jpg")}
-                        style={{ width: "80%", height: 250, marginTop: "20%", alignSelf: 'center', resizeMode: 'contain' }}
-                    /> */}
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
 
-                    <Text style={{ fontsize: 22, fontWeight: 'bold', marginLeft: '5%', fontFamily:"OldEnglish" }}>Login</Text>
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
 
-                    <View style={{ flexDirection: 'row', width: '90%', alignSelf: 'center', marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
-                        <Feather name="at-sign" size={20} color="black" />
-                        <TextInput style={{ width: '90%', height: 30, borderBottomWidth: 1, paddingLeft: 10, borderBottomColor: "gray" }} placeholder="Email ID" />
-                    </View>
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <ImageBackground
+            source={require('../assets/Background.jpg')}
+            style={{
+              flex: 1,
+              resizeMode: 'contain',
+            }}>
+            <ScrollView>
+              <Image
+                source={require('../assets/Logo.png')}
+                style={{
+                  width: '90%',
+                  height: 250,
+                  marginTop: '20%',
+                  alignSelf: 'center',
+                  resizeMode: 'contain',
+                }}
+              />
 
-                    <View style={{ flexDirection: 'row', width: '90%', alignSelf: 'center', marginTop: 30, justifyContent: 'center', alignItems: 'center' }}>
-                        <AntDesign name="lock" size={24} color="black" />
-                        <TextInput style={{ width: '85%', height: 30, borderBottomWidth: 1, paddingLeft: 10, borderBottomColor: "gray" }} placeholder="Password" />
-                        <Feather name="eye-off" size={24} color="gray" />
-                    </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  width: '90%',
+                  alignSelf: 'center',
+                  marginTop: 30,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <FontAwesome name="at" size={24} color="white" />
+                <TextInput
+                  style={{
+                    width: '80%',
+                    height: 30,
+                    borderBottomWidth: 2,
+                    paddingLeft: 10,
+                    borderBottomColor: 'white',
+                  }}
+                  placeholder="Email ID"
+                  placeholderTextColor="silver"
+                />
+              </View>
 
-                    <Text style={{ marginTop: 10, color: 'blue', fontWeight: 'bold', alignSelf: 'flex-end', marginHorizontal: '5%' }}>Forgot Password?</Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  width: '90%',
+                  alignSelf: 'center',
+                  marginTop: 30,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <MaterialCommunityIcons name="lock" size={24} color="white" />
+                <TextInput
+                  style={{
+                    width: '72%',
+                    height: 30,
+                    borderBottomWidth: 2,
+                    paddingLeft: 10,
+                    borderBottomColor: 'white',
+                  }}
+                  placeholder="Password"
+                  placeholderTextColor="silver"
+                />
+                <Feather name="eye-off" size={24} color="white" />
+              </View>
 
-                    <TouchableOpacity style={{ backgroundColor: 'blue', width: '90%', height: 40, marginTop: 30, borderRadius: 10, alignSelf: 'center', justifyContent: 'center', alignItems: 'center' }}>
-                        <Text style={{ fontSize: 10, color: 'white' }}>Login</Text>
-                    </TouchableOpacity>
+              <Text
+                style={{
+                  marginTop: 10,
+                  color: 'violet',
+                  fontWeight: 'bold',
+                  alignSelf: 'flex-end',
+                  marginHorizontal: '5%',
+                }}> Forgot Password?
+              </Text>
 
-                    <Text style={{ color: 'gray', alignSelf: 'center', marginTop: 30 }}>OR</Text>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: 'lime',
+                  width: '70%',
+                  height: 40,
+                  marginTop: 30,
+                  borderRadius: 10,
+                  alignSelf: 'center',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Text
+                  style={{
+                    fontSize: 30,
+                    color: 'blue',
+                    fontFamily: 'OldEnglish',
+                  }}>
+                  Login
+                </Text>
+              </TouchableOpacity>
 
-                    <TouchableOpacity style={{ flexDirection: 'row', alignSelf: 'center', backgroundColor: '#ccc', width: '90%', height: 40, borderRadius: 10, justifyContent: 'center', alignItems: 'center', marginTop: 30 }}>
-                        <Image
-                            style={{ width: 30, height: 30 }}
-                            source={require("../assets/Google.png")} />
-                        <Text style={{ marginLeft: 10, color: 'gray' }}>Login with Google</Text>
-                    </TouchableOpacity>
+              <Text
+                style={{
+                  color: '',
+                  alignSelf: 'center',
+                  marginTop: 30,
+                  fontSize: 20,
+                }}>
+                OR
+              </Text>
 
-                    <Text style={{ alignSelf: 'center', marginTop: 30, color: 'gray' }}>New to Logistics?<Text style={{ color: 'blue', fontWeight: 'bold' }}> Register</Text> </Text>
+              <TouchableOpacity
+                style={{
+                  flexDirection: 'row',
+                  alignSelf: 'center',
+                  backgroundColor: 'azure',
+                  width: '90%',
+                  height: 40,
+                  borderRadius: 10,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  marginTop: 30,
+                }}>
+                <Image
+                  style={{ width: 40, height: 40, marginLeft: '1%' }}
+                  source={require('../assets/Google.png')}
+                />
+                <Text
+                  style={{
+                    marginLeft: 15,
+                    color: 'green',
+                    fontFamily: 'OldEnglish',
+                    fontSize: 30,
+                  }}>
+                  Login with Google
+                </Text>
+              </TouchableOpacity>
 
-                </ScrollView>
-            </View>
-        )
+              <Text
+                style={{
+                  alignSelf: 'center',
+                  marginTop: 30,
+                  marginBottom: '10%',
+                  color: 'white',
+                  fontSize: '95%'
+                }}>
+                Don't have an account?
+                <TouchableOpacity>
+                <Text
+                  style={{
+                    color: 'blue',
+                    fontWeight: 'bold',
+                  }}>  Sign Up
+                </Text>
+                </TouchableOpacity>
+              </Text>
+            </ScrollView>
+          </ImageBackground>
+        </View>
+      );
     }
-   }
- }
+  }
+}
