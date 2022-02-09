41a305ff014b58d6620ce9fd6b27671e33ec4d01
Preetham869 <70512182+Preetham869@users.noreply.github.com>
2022-02-09 20:49:40 +0530

    Add files via upload

---
+++ screens/Profile.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+let customFonts = {
+  OldEnglish: require('../assets/OldeEnglish.ttf'),
+};
+
+export default class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <ScrollView>
+            <Text>Profile Screen</Text>
+          </ScrollView>
+        </View>
+      );
+    }
+  }
+}
---
+++ screens/SignUp.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+let customFonts = {
+  OldEnglish: require('../assets/OldeEnglish.ttf'),
+};
+
+export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+         <ImageBackground
+            source={require('../assets/bg.jpg')}
+            style={{
+              flex: 1,
+              resizeMode: 'contain',
+            }}>
+          <ScrollView>
+          
+          </ScrollView>
+          </ImageBackground>
+        </View>
+      );
+    }
+  }
+}
