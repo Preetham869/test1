91b99aa58a28a9e0aed8dfcb930de0a2d4a70968
Preetham869 <70512182+Preetham869@users.noreply.github.com>
2022-02-10 10:22:57 +0530

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -8,45 +8,227 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  SectionList
 } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { Feather } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-// import * as Font from 'expo-font';
-// import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
-// let customFonts = {
-//   OldEnglish: require('../assets/OldeEnglish.ttf'),
-// };
+let customFonts = {
+  OldEnglish: require('../assets/OldeEnglish.ttf'),
+};
+
+const ListItem = ({ item }) => {
+  return (
+    <View style={styles.item}>
+      <Image
+        source={{
+          uri: item.uri,
+        }}
+        style={styles.itemPhoto}
+        resizeMode="cover"
+      />
+      <Text style={styles.itemText}>{item.text}</Text>
+    </View>
+  );
+};
+
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    //   fontsLoaded: false,
+      fontsLoaded: false,
     };
   }
 
-//   async _loadFontsAsync() {
-//     await Font.loadAsync(customFonts);
-//     this.setState({ fontsLoaded: true });
-//   }
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
 
-//   componentDidMount() {
-//     this._loadFontsAsync();
-//   }
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
 
   render() {
-    // if (!this.state.fontsLoaded) {
-    //   return <AppLoading />;
-    // } else {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
       return (
+//         <View style={styles.container}>
+//       <StatusBar style="light" />
+//       <SafeAreaView style={{ flex: 1 }}>
+//         <SectionList
+//           contentContainerStyle={{ paddingHorizontal: 10 }}
+//           stickySectionHeadersEnabled={false}
+//           sections={SECTIONS}
+//           renderSectionHeader={({ section }) => (
+//             <>
+//               <Text style={styles.sectionHeader}>{section.title}</Text>
+//               {section.horizontal ? (
+//                 <FlatList
+//                   horizontal
+//                   data={section.data}
+//                   renderItem={({ item }) => <ListItem item={item} />}
+//                   showsHorizontalScrollIndicator={false}
+//                 />
+//               ) : null}
+//             </>
+//           )}
+//           renderItem={({ item, section }) => {
+//             if (section.horizontal) {
+//               return null;
+//             }
+//             return <ListItem item={item} />;
+//           }}
+//         />
+//       </SafeAreaView>
+//     </View>
+//   );
+// };
+
+// const SECTIONS = [                                                            // Creating sections for horizontal
+//   {
+//     title: 'Made for you',
+//     horizontal: true,
+//     data: [
+//       {
+//         key: '1',
+//         text: 'Item text 1',
+//         uri: 'https://picsum.photos/id/1/200',
+//       },
+//       {
+//         key: '2',
+//         text: 'Item text 2',
+//         uri: 'https://picsum.photos/id/10/200',
+//       },
+
+//       {
+//         key: '3',
+//         text: 'Item text 3',
+//         uri: 'https://picsum.photos/id/1002/200',
+//       },
+//       {
+//         key: '4',
+//         text: 'Item text 4',
+//         uri: 'https://picsum.photos/id/1006/200',
+//       },
+//       {
+//         key: '5',
+//         text: 'Item text 5',
+//         uri: 'https://picsum.photos/id/1008/200',
+//       },
+//     ],
+//   },                                                              // Creating sections for Vertical FlatList
+//   {
+//     title: 'Punk and hardcore',
+//     data: [
+//       {
+//         key: '1',
+//         text: 'Item text 1',
+//         uri: 'https://picsum.photos/id/1011/200',
+//       },
+//       {
+//         key: '2',
+//         text: 'Item text 2',
+//         uri: 'https://picsum.photos/id/1012/200',
+//       },
+
+//       {
+//         key: '3',
+//         text: 'Item text 3',
+//         uri: 'https://picsum.photos/id/1013/200',
+//       },
+//       {
+//         key: '4',
+//         text: 'Item text 4',
+//         uri: 'https://picsum.photos/id/1015/200',
+//       },
+//       {
+//         key: '5',
+//         text: 'Item text 5',
+//         uri: 'https://picsum.photos/id/1016/200',
+//       },
+//     ],
+//   },
+//   {
+//     title: 'Based on your recent listening',
+//     data: [
+//       {
+//         key: '1',
+//         text: 'Item text 1',
+//         uri: 'https://picsum.photos/id/1020/200',
+//       },
+//       {
+//         key: '2',
+//         text: 'Item text 2',
+//         uri: 'https://picsum.photos/id/1024/200',
+//       },
+
+//       {
+//         key: '3',
+//         text: 'Item text 3',
+//         uri: 'https://picsum.photos/id/1027/200',
+//       },
+//       {
+//         key: '4',
+//         text: 'Item text 4',
+//         uri: 'https://picsum.photos/id/1035/200',
+//       },
+//       {
+//         key: '5',
+//         text: 'Item text 5',
+//         uri: 'https://picsum.photos/id/1038/200',
+//       },
+//     ],
+//   },
+// ];
+
         <View style={{ flex: 1 }}>
+          <ImageBackground
+            source={require('../assets/bgHome.jpg')}
+            style={{
+              flex: 1,
+              resizeMode: 'center',
+            }}>
           <ScrollView>
-            <Text>Home Screen</Text>
+            
           </ScrollView>
+        </ImageBackground>
         </View>
-      );
-    }
+      
+      )
+    }  
   }
-// }
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#121212',
+//   },
+//   sectionHeader: {
+//     fontWeight: '800',
+//     fontSize: 18,
+//     color: '#f4f4f4',
+//     marginTop: 20,
+//     marginBottom: 5,
+//   },
+//   item: {
+//     margin: 10,
+//   },
+//   itemPhoto: {
+//     width: 200,
+//     height: 200,
+//   },
+//   itemText: {
+//     color: 'rgba(255, 255, 255, 0.5)',
+//     marginTop: 5,
+//   },
+//   });
